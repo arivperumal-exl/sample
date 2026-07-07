974a5ab6b2b3821a3c3a00fa2d72cf025cfe77b2
arivperumal-exl <Arivarasu.Perumal@exlservice.com>
2026-07-07 10:57:32 +0000

    Update and rename Misc.py to Misc.txt

--- Misc.py
+++
@@ -1,9 +0,0 @@
-# unwrap the list (batch_size=1), element may be None or a tensor
-mask_item = mask[0] if isinstance(mask, list) else mask
-if mask_item is not None:
-    if torch.is_tensor(mask_item):
-        m = mask_item.squeeze().numpy()
-    else:
-        m = np.array(mask_item)
-    pixel_scores.append(amap.ravel())
-    pixel_labels.append((m > 0).ravel().astype(int))
