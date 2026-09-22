8ee8e1bee88b1159564e359c68947b265da60b10
arivperumal-exl <Arivarasu.Perumal@exlservice.com>
2026-09-22 10:49:48 +0000

    Refactor supervisor_predict_fn to use ChatMessage

--- transfer.py
+++ transfer.py
@@ -1,10 +1,13 @@
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.serving import ChatMessage, ChatMessageRole
 
 w = WorkspaceClient()
 
 def supervisor_predict_fn(question: str) -> dict:
     response = w.serving_endpoints.query(
-        name="your-supervisor-endpoint-name",  # TODO: your actual endpoint name
-        messages=[{"role": "user", "content": question}],
+        name="your-supervisor-endpoint-name",  # <-- put your actual endpoint name here
+        messages=[
+            ChatMessage(role=ChatMessageRole.USER, content=question)
+        ],
     )
     return {"response": response.choices[0].message.content}
