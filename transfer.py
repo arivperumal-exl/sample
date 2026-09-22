d4ac5b2ed4b4743c136919c1abad2ee79e746484
arivperumal-exl <Arivarasu.Perumal@exlservice.com>
2026-09-22 08:13:55 +0000

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,10 +1,3 @@
-import mlflow
+import mlflow.genai.datasets
 
-mlflow.set_experiment("/Users/you@company.com/genie-supervisor-eval")
-
-with mlflow.start_run(run_name="supervisor_eval_v1"):
-    results = mlflow.genai.evaluate(
-        data=dataset,
-        predict_fn=supervisor_predict_fn,
-        scorers=[correct_routing, sql_correctness, out_of_scope_handling, out_of_scope_disclosed_first],
-    )
+dataset = mlflow.genai.datasets.get_dataset(name="catalog.schema.your_eval_dataset")
